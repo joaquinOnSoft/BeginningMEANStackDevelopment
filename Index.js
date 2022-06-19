8a2e25b529984450d60ed7195187c6402b099f78
JoaquinOnSoft <joaquingp@gmail.com>
2022-06-19 15:22:24 +0200

    Chapter 6

---
+++ Index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import cors from 'cors';
+import MoviesRoute from './api/MoviesRoute.js';
+import dotenv from 'dotenv';
+import mongodb from 'mongodb';
+
+class Index {
+	static app = express();
+
+	static router = express.Router();
+
+	static main(){
+		dotenv.config();
+		Index.setUpServer();
+		Index.setUpDatabase();
+	}
+
+	static async setUpDatabase(){
+		const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_UI);
+		try{
+			//Connect to MongoDB Cluster
+			await client.connect();
+		} catch (e){
+			console.error(e);
+			process.exit(-1);
+		}
+	}
+
+	static async setUpServer(){
+		Index.app.use(cors());
+		Index.app.use(express.json());
+		Index.app.use('/api/v1/movies', MoviesRoute.configRoutes(Index.router));
+		Index.app.use('*', (req, res) => {
+			res.status(404).json({error: 'not found'});
+		})
+
+		const port = process.env.PORT || 8000;
+
+		try{
+			Index.app.listen(port, () => {
+				console.log(`Server us running on port ${port}`);
+			});
+		} catch (e){
+			console.error(e);
+			process.exit(-1);
+		}
+	}
+}
+
+Index.main();
---
+++ api/MoviesRoute.js
@@ -0,0 +1,6 @@
+export default class MoviesRoute {
+	static configRoutes(router){
+		router.route('/').get((req, res) => res.send('Hello World!'));
+		return router;
+	}
+}
