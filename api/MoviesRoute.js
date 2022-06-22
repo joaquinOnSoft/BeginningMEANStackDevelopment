e47ba910bb9f2ba73e3fd72548e860fa359ccc61
JoaquinOnSoft <joaquingp@gmail.com>
2022-06-22 19:13:22 +0200

    Added comments to explain API method

--- api/MoviesRoute.js
+++ api/MoviesRoute.js
@@ -1,8 +1,19 @@
 import MoviesController from './MoviesController.js';
 
 export default class MoviesRoute {
+	/**
+	 * Some URL exampmples:
+	 * <strong>/api/v1/movies</strong> Recover all movies that match
+	 * the given filter
+	 * <ul>
+	 *     <li>http://localhost:5000/api/v1/movies</li>
+	 *     <li>http://localhost:5000/api/v1/movies?title=Dragon</li>
+	 *     <li>http://localhost:5000/api/v1/movies?rated=G</li>
+	 *     <li>http://localhost:5000/api/v1/movies?rated=G&page=2</li>
+	 * </ul>
+	 * 
+	 */
 	static configRoutes(router){
-		//router.route('/').get((req, res) => res.send('Hello World!'));
 		router.route('/').get(MoviesController.apiGetMovies);
 		return router;
 	}
