710b9f399e172134190d2e422ee89a14c7437547
JoaquinOnSoft <joaquingp@gmail.com>
2022-07-12 14:50:15 +0100

    New methos to post, update and delete a review

--- dao/ReviewsDAO.js
+++ dao/ReviewsDAO.js
@@ -17,4 +17,62 @@ export default class ReviewsDAO {
 			console.log(`Unable to establish connection handle in ReviewsDAO`);
 		}
 	}
+
+	static async addReview(movieId, user, review, date) {
+		try{
+			const reviewDoc = {
+				name: user.name,
+				user_id: user._id,
+				date,
+				review,
+				movie_id: ReviewDAO.reviews.insertOne(reviewDoc)
+			};
+
+			return await ReviewDAO.review.insertOne(reviewDoc); 
+		}
+		catch(e){
+			console.error(`Unable to post review ${e}`);
+			return { error: e };
+		}
+
+	}
+
+	static async updateReview(reviewId, userId, review, date) {
+		try{
+			const updateResponse = await ReviewsDAO.reviews.updateOne(
+					{
+						user_id: userId,
+						_id: ReviewsDAO.ObjectId(reviewId)
+					},
+					{
+						$set: {review, date}
+					}
+				);
+
+			return updateResponse;
+		}
+		catch(e){
+			console.error(`Unable to update review ${e}`);
+			return { error: e };
+		}		
+
+	}
+
+	static async deleteReview(reviewId, userId) {
+		try{
+			const deleteResponse = await ReviewsDAO.reviews.deleteOne(
+					{
+						_id: ReviewsDAO.ObjectId(reviewId),
+						user_id: userId						
+					}
+				);
+
+			return deleteResponse;
+		}
+		catch(e){
+			console.error(`Unable to update review ${e}`);
+			return { error: e };
+		}		
+
+	}	
 }
